1173f500f23988bf03731a1d1f0b177e082047fb
andrebaroni <andrelbaroni1@gmail.com>
2019-02-07 10:43:31 -0200

    estudos

---
+++ frontend/src/pages/Timeline.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import twitterLogo from '../twitter.svg'
+import './Timeline.css'
+import api from '../services/api';
+
+export default class Timeline extends Component {
+
+    state = {
+        newTweet: '',
+    }
+
+    handleInputChange = e => {
+        this.setState({ newTweet: e.target.value })
+    }
+
+
+    handleNewTweet = async e => {
+        if(e.keyCode == 13) {
+            const content = this.state.newTweet
+            const author = localStorage.getItem('@GoTwitter:username')
+
+            await api.post('tweets', { content, author })
+
+            this.setState({ newTweet: "" })
+            
+            return 
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontecendo?"
+                    />
+                </form>
+
+                {/* {<ul>
+
+
+                </ul>} */}
+
+            </div>
+        )
+    }
+}
